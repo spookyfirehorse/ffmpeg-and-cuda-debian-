1c37648e80c80de0230af67da3ca0efaf3b79b5e
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2024-05-03 13:27:38 +0200

    Delete mpv-cuda.sh

--- mpv-cuda.sh
+++
@@ -1,6 +0,0 @@
-sudo apt build-dep mpv && cd ~/ffmpeg_sources  && \
-git clone https://github.com/mpv-player/mpv.git && \
-cd ~/ffmpeg_sources/mpv && \
-./bootstrap.py  && ./waf configure  --enable-cuda-interop --enable-cuda-hwaccel --enable-egl-drm --enable-gl-x11 --enable-egl --enable-sdl2    \
---enable-xv  --enable-egl-x11 --enable-libplacebo --enable-vulkan  --enable-zimg --enable-ffmpeg-strict-abi  \
-./waf -j4 && sudo  ./waf install 
