e2fcee4c4d76652d90fda590cd9fe0f5485a74eb
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2024-06-07 09:36:00 +0200

    Update handbrake.sh

--- handbrake.sh
+++ handbrake.sh
@@ -2,4 +2,4 @@ git clone https://github.com/HandBrake/HandBrake.git && cd HandBrake
 
 ./configure --enable-gtk --enable-fdk-aac --enable-nvenc --enable-nvdec    --launch-jobs=$(nproc) --launch
 
-sudo make --directory=build uninstall
+sudo make --directory=build install
