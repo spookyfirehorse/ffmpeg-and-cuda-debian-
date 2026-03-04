4cc00fc49eca594294e2d3b9a9d65cd5f3db5e6e
spooky <47276604+spookyfirehorse@users.noreply.github.com>
2026-03-04 18:48:07 +0100

    Update ffconvert-mkv-cuda.sh

--- ffconvert-mkv-cuda.sh
+++ ffconvert-mkv-cuda.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
-for file in "$1"; do ffmpeg  -hwaccel cuda -hwaccel_output_format nv12   -i "$file"  -c:v h264_nvenc    -preset p6 -tune hq -b:v 5M -bufsize 5M -maxrate 10M -qmin 0 -g 250 -bf 3 -b_ref_mode middle -temporal-aq 1 -rc-lookahead 20 -i_qfactor 0.75 -b_qfactor 1.1      -c:a libfdk_aac  -b:a 96k -f matroska "${file%.*}_high.mkv"; done
+for file in "$1"; do
+  echo ">>> Prüfe & Repariere AVI-Struktur für NVENC: $file"
+  
+  # SCHRITT 1: Software-Fix (Packed B-Frames & Interleaving)
+  # Erstellt einen sauberen Stream für die NVIDIA Karte
+  ffmpeg -y -i "$file" -c copy -bsf:v mpeg4_unpack_bframes "temp_repair.mkv" -loglevel error
 
-# -preset slow  -bufsize 5M  -b:v 2M -preset slow   -filter:v scale=1280:-1 
+  if [ $? -eq 0 ]; then
+    echo ">>> Starte High-Quality NVENC Encoding..."
 
-# -s 720x480
+    # SCHRITT 2: Dein High-Quality NVENC Setup
+    ffmpeg -hwaccel cuda -hwaccel_output_format nv12 -i "temp_repair.mkv" \
+      -c:v h264_nvenc -preset p6 -tune hq -b:v 5M -bufsize 5M -maxrate 10M \
+      -qmin 0 -g 250 -bf 3 -b_ref_mode middle -temporal-aq 1 -rc-lookahead 20 \
+      -i_qfactor 0.75 -b_qfactor 1.1 \
+      -c:a libfdk_aac -b:a 96k \
+      -f matroska "${file%.*}_high.mkv" -y
 
-#find . -name *.avi -or -name *.mkv -or -name *.mp4 -or -name *flv -exec ffmpeg  -hwaccel cuda -hwaccel_output_format nv12  -i {} -c:v h264_nvenc   -preset slow     -filter:v scale=1280:-1    -c:a libfdk_aac  -b:a 96k -f matroska  -c:a libfdk_aac  -b:a 96k -f matroska {}_high.mkv \;
-
+    rm "temp_repair.mkv"
+    echo ">>> Fertig: ${file%.*}_high.mkv"
+  else
+    echo "!!! Reparatur fehlgeschlagen !!!"
+  fi
+done
